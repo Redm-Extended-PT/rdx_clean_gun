05636dfb27dfcc59749e08b4c04c318cce2c0b83
Vision <77315942+ValiantCraven27@users.noreply.github.com>
2022-09-25 20:39:52 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,4 @@
-fx_version "adamant"
+fx_version "cerulean"
 games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
@@ -13,4 +13,4 @@ client_scripts {
 	'client/main.lua'
 }
 
-dependency 'vorp_core'
+dependency {}
